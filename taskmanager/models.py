2806683f2b9c1b6b56f6393dc5d0f87cb65dddd9
Ben Smith <bens6477@gmail.com>
2022-02-01 17:34:10 +0000

    Add functionality to view tasks

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -4,7 +4,7 @@ from taskmanager import db
 class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
-    category_name = db.Column(db.String(25), unique=True, nullable=False)
+    category_name = db.Column(db.String(50), unique=True, nullable=False)
     tasks = db.relationship(
         "Task", backref="category", cascade="all, delete", lazy=True)
 
@@ -16,7 +16,7 @@ class Category(db.Model):
 class Task(db.Model):
     # schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
-    task_name = db.Column(db.String(25), unique=True, nullable=False)
+    task_name = db.Column(db.String(50), unique=True, nullable=False) # Not been re-initializes - still String(25)
     task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
